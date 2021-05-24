edcfe1ecd6c1d04d009fe558ce33bbdb114f44bb
lizhiying <tim.doct@gmail.com>
2021-05-24 13:54:47 +0800

    FIX: build issue

--- example/android/app/src/main/java/com/codeheadlabs/zendeskexample/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/codeheadlabs/zendeskexample/EmbeddingV1Activity.java
@@ -1,7 +1,6 @@
 package com.codeheadlabs.zendeskexample;
 
 import android.os.Bundle;
-import com.codeheadlabs.zendesk.ZendeskPlugin;
 import dev.flutter.plugins.e2e.E2EPlugin;
 import io.flutter.app.FlutterActivity;
 
@@ -10,7 +9,7 @@ public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ZendeskPlugin.registerWith(registrarFor("com.codeheadlabs.zendesk.ZendeskPlugin"));
+    android.src.main.java.com.codeheadlabs.zendesk.ZendeskPlugin.registerWith(registrarFor("com.codeheadlabs.zendesk.ZendeskPlugin"));
     E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
   }
 }
